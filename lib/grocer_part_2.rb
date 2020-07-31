3d221ad023be6f952baf8a610e724c2cc5c0f140
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-31 13:29:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,7 +3,7 @@ require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
   #cart is an array of hashes (structure of the consolidated cart)
   #coupons is an array of coupon hashes (:item => which item, :num => how many the coupon applies to, :cost => new cost)
-  #return new array that looks like (single avocado item at normal price, normal kale, two avocado items at discounted price)
+  #return new array that looks like (single avocado item at normal price, normal kale, avocado with coupon count 2)
 end
 
 def apply_clearance(cart)
